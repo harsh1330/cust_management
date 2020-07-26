88757f434501c5ffa172a0bab7fa8570018d6fc1
harsh1330 <hgupta7629@gmail.com>
2020-07-27 01:53:07 +0530

    add formset in place order of customer

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from accounts.models import Order, Customer
+from django.forms import inlineformset_factory
+
 from .forms import OrderForm
 from django.forms import inlineformset_factory
 
@@ -45,16 +47,19 @@ def products(request):
     return render(request, 'accounts/products.html',context)
 
 def create_order(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status'), extra=5) #extra is here to add multiple orders a same time. Extra shows how much empty fields at a single time display
     customer = Customer.objects.get(id=pk)
-    form = OrderForm(initial={'customer':customer})
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer) # Here queryset=Order.objects.none() is used so that no saved orders are going to store in formset and they will now be displayed in the form of order_form.html
+    #form = OrderForm(initial={'customer':customer})
 
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        formset = OrderFormSet(request.POST, instance=customer)
+        #form = OrderForm(request.POST)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
-    context ={'form' : form}
+    context ={'formset' : formset}  #used formset here for multiple forms. Use *form* in context instead of formset and in order_form.html also to use a single form
     return render(request, 'accounts/order_form.html', context)
 
 def update_order(request, pk):
